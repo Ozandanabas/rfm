9e2c68d8d64045cb7b6be1ce7489c95e42d3f1f9
Ozandanabas <75380815+Ozandanabas@users.noreply.github.com>
2021-12-16 16:00:26 +0300

    added comments

--- RFM.py
+++ RFM.py
@@ -4,52 +4,33 @@ from sklearn.preprocessing import MinMaxScaler
 pd.set_option("display.max_columns", None)
 pd.set_option("display.float_format", lambda x: '%.5f' % x)
 ################################################################
-# Veriyi Anlama ve Hazırlama
+# data analysing and data process
 ################################################################
-
-# Görev1.1
 
 df_ = pd.read_excel("dataset/online_retail_II.xlsx", sheet_name="Year 2010-2011")
 df = df_.copy()
 
-# Görev1.2
-
 df.describe().T
-
-# Görev1.3
 
 df.isnull().sum()
 
-# Görev1.4
-
 df.dropna(inplace = True)
-
-# Görev1.5
 
 df["StockCode"].nunique()
 
-# Görev1.6
-
 df["StockCode"].value_counts()
 
-# Görev1.7
-
 df.groupby("Description").agg({"Quantity":"sum"}).sort_values("Quantity", ascending=False)
-
-# Görev1.8
-
 df[~df["Invoice"].str.contains("C", na = False)]
-
-# Görev1.9
 
 df["TotalPrice"] = df["Quantity"] * df["Price"]
 df.head()
 
 ################################################################
-# RFM metriklerinin hesaplanması
+# RFM calculation of metrics
 ################################################################
 
-# Görev2
+
 today_date = dt.datetime(2011,12,11)
 rfm = df.groupby("Customer ID").agg({"InvoiceDate": lambda InvoiceDate: (today_date-InvoiceDate.max()).days,
                                     "Invoice": lambda Invoice: Invoice.nunique(),
@@ -59,10 +40,10 @@ rfm.columns = ["Recency","Frequency","Monetary"]
 rfm = rfm.loc[(rfm["Monetary"] > 0)]
 
 ################################################################
-# RFM skorlarının oluşturulması ve tek bir değişkene çevrilmesi
+# RFM make scores ve converting to a single variable
 ################################################################
 
-# Görev3
+
 
 rfm["recency_score"] = pd.qcut(rfm["Recency"], 5, labels = (5,4,3,2,1))
 rfm["frequency_score"] = pd.qcut(rfm["Frequency"].rank(method = "first"), 5, labels = (1,2,3,4,5))
@@ -71,10 +52,9 @@ rfm["RFM_SCORE"] = (rfm["recency_score"].astype(str) +
                     rfm["frequency_score"].astype(str))
 
 ################################################################
-# RFM skorlarının segment olarak tanımlanması
+# RFM definition of scores as segments
 ################################################################
 
-# Görev4
 
 seg_map = {
     r'[1-2][1-2]': 'hibernating',
@@ -93,10 +73,9 @@ rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
 
 
 ################################################################
-# Aksiyon Zamanı
+# Action time
 ################################################################
 
-# Görev5
 
 rfm.loc[(rfm["segment"] == "at_Risk") | (rfm["segment"] == "need_attention") | (rfm["segment"] == "hibernating")].groupby("segment").agg({"mean","std"})
 new_df = pd.DataFrame()
@@ -118,7 +97,7 @@ rfm.head()
 # 8. Segmentlerin Oluşturulması
 
 ##################################################
-# 1. Veri Hazırlama
+# 1. Data Processing
 ##################################################
 
 cltv_c = df.groupby(["Customer ID"]).agg({"Invoice": lambda x: x.nunique(),
@@ -170,7 +149,7 @@ scaler.fit(cltv_c[["CLTV"]])
 cltv_c["scaled_cltv"] = scaler.transform(cltv_c[["CLTV"]])
 
 # ##################################################
-# 8.Segmentlerin Oluşturulması
+# 8.Creating Segments
 # ##################################################
 
 cltv_c["segment"] = pd.qcut(cltv_c["scaled_cltv"], 4, labels=("D", "C","B","A"))
